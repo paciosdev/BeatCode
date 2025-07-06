d37f73607b503ac2baf2211bab616d80d7488142
Francesco Paciello <kekkopacio@gmail.com>
2025-07-06 16:00:20 +0200

    Initial Commit

---
+++ BeatCode/BeatCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatCodeApp.swift
+//  BeatCode
+//
+//  Created by Francesco Paciello on 06/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BeatCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
